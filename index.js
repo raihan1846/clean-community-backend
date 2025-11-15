446b5f85ec217df7723ad6cc8c6010a98bb8100d
raihan1846 <raihan.bd2022@gmail.com>
2025-11-15 17:13:49 +0600

    create mongodb user, create client and uri create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,7 +9,17 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://raihan:raihan@cluster0.7ziiyhp.mongodb.net/?appName=Cluster0";
 
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  
 app.get('/', (req, res) => {
     res.send('Clean Community is Running..!!!')
   })
